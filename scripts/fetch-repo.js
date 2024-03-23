1fbfcdefb7cc418f526d9ba3440be8ead083a687
Sergio Ribera <56278796+SergioRibera@users.noreply.github.com>
2024-03-23 00:15:18 -0400

    chore: add github token to get API

--- scripts/fetch-repo.js
+++ scripts/fetch-repo.js
@@ -1,7 +1,20 @@
 import fetch from "node-fetch"
+import langsColor from "./langColors.json" with { "type": "json" }
+import { env } from "process";
+
+const GH_TOKEN = env.GH_TOKEN;
 
 const fetchRepo = async (username, reponame) => {
-    let res = await fetch(`https://api.github.com/repos/${username}/${reponame}`);
+    let res = await fetch(`https://api.github.com/repos/${username}/${reponame}`, {
+        headers: {
+            "Authorization": `token ${GH_TOKEN}`,
+        }
+    });
+
+    if (!res.ok) {
+        throw new Error(`Cannot get "${username}/${reponame}" repository: ${await res.text()}`)
+    }
+
     let data = await res.json()
 
     if (!data) {
@@ -9,14 +22,17 @@ const fetchRepo = async (username, reponame) => {
     }
 
     if (!data.html_url || data.private) {
-        throw new Error("User Repository Not found");
+        throw new Error(`User Repository Not found - ${data.html_url} - ${data.private}`);
     }
 
     return {
         name: data.name,
         nameWithOwner: data.full_name,
         description: data.description,
-        primaryLanguage: data.language,
+        primaryLanguage: {
+            name: data.language,
+            color: langsColor[data.language] || "#2f80ed",
+        },
         isArchived: data.archived,
         isTemplate: data.is_template,
         starCount: data.stargazers_count,
